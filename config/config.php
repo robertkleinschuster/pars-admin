b5a9ce55889b6e91f6a124dbc24cff0e01f8817c
robertkleinschuster <robert.kleinschuster@niceshops.com>
2020-11-29 11:45:59 +0100

    dependencies

--- config/config.php
+++ config/config.php
@@ -55,7 +55,6 @@ $aggregator = new ConfigAggregator([
     //   - `local.php`
     //   - `*.local.php`
     new PhpFileProvider(realpath(__DIR__) . '/autoload/{{,*.}global,{,*.}local}.php'),
-
     // Load development config if it exists
     new PhpFileProvider(realpath(__DIR__) . '/development.config.php'),
 ], $cacheConfig['config_cache_path']);
