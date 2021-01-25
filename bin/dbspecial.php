8cb03b41154b8a2f0dd2a2d01f299d0a01090c5c
robertkleinschuster <robert.kleinschuster@niceshops.com>
2021-01-25 20:54:27 +0100

    special update deploy action

---
+++ bin/dbspecial.php
@@ -0,0 +1,17 @@
+<?php
+chdir(dirname(__DIR__));
+require 'vendor/autoload.php';
+
+/**
+ * Self-called anonymous function that creates its own scope and keeps the global namespace clean.
+ */
+(function () {
+
+    /** @var \Psr\Container\ContainerInterface $container */
+    $container = require __DIR__ . '/../config/container.php';
+    $adapter = $container->get(\Laminas\Db\Adapter\AdapterInterface::class);
+
+    $schemaUpdater = new \Pars\Core\Database\Updater\SpecialUpdater($adapter);
+    $schemaUpdater->executeSilent();
+    echo 'Special Updated';
+})();
