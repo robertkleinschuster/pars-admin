621fc0d55ad9795e6e38aad2d78553c88be053d6
robertkleinschuster <robert.kleinschuster@niceshops.com>
2021-04-07 21:40:10 +0200

    updates

--- src/ApplicationFactory.php
+++ src/ApplicationFactory.php
@@ -6,6 +6,7 @@ use Laminas\HttpHandlerRunner\RequestHandlerRunner;
 use Mezzio\Application;
 use Mezzio\MiddlewareFactory;
 use Mezzio\Router\RouteCollector;
+use Pars\Core\Deployment\UpdateHandler;
 use Psr\Container\ContainerInterface;
 
 class ApplicationFactory extends \Mezzio\Container\ApplicationFactory
@@ -18,6 +19,12 @@ class ApplicationFactory extends \Mezzio\Container\ApplicationFactory
             $container->get(RouteCollector::class),
             $container->get(RequestHandlerRunner::class)
         );
+        register_shutdown_function(function () use ($container) {
+            $error = error_get_last();
+            if (isset($error['type']) && $error['type'] === E_ERROR) {
+                UpdateHandler::handleAppUpdate($container);
+            }
+        });
         $factory = $container->get(\Mezzio\MiddlewareFactory::class);
         (require realpath(implode(DIRECTORY_SEPARATOR, [__DIR__, '..', 'config', 'pipeline.php'])))($app, $factory, $container);
         (require realpath(implode(DIRECTORY_SEPARATOR, [__DIR__, '..', 'config', 'routes.php'])))($app, $factory, $container);
