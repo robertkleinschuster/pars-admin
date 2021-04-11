9c50219ec3f7256d156addbc0c650e4489953ee0
robertkleinschuster <robert.kleinschuster@niceshops.com>
2021-04-11 18:12:33 +0200

    updates

--- src/ApplicationContainerFactory.php
+++ src/ApplicationContainerFactory.php
@@ -4,20 +4,14 @@ declare(strict_types=1);
 
 namespace Pars\Admin;
 
+use Pars\Core\Deployment\CacheClearer;
+
 class ApplicationContainerFactory
 {
     public function __invoke()
     {
         $config = $this->getApplicationConfig();
-        register_shutdown_function(function () use ($config) {
-            $error = error_get_last();
-            if (isset($error['type']) && $error['type'] === E_ERROR) {
-                if (isset($config['config_cache_path']) && file_exists($config['config_cache_path'])) {
-                    unlink($config['config_cache_path']);
-                    syslog(LOG_ERR, 'cleared config cache');
-                }
-            }
-        });
+        CacheClearer::registerConfigCacheFunction($config);
         $dependencies = $config['dependencies'];
         $dependencies['services']['config'] = $config;
         return new ApplicationContainer($dependencies);
