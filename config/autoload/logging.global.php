739b7107791f83c9e9ee3f2dabda9291f687ba51
robertkleinschuster <robert.kleinschuster@niceshops.com>
2021-04-12 22:08:49 +0200

    clean up laminas dependencies

--- config/autoload/logging.global.php
+++ config/autoload/logging.global.php
@@ -3,42 +3,11 @@
 return [
     'psr_log' => [
         'Logger' => [
-            'exceptionhandler' => true,
-            'errorhandler' => true,
-            'fatal_error_shutdownfunction' => true,
             'writers' => [
                 'syslog' => [
-                    'name' => 'syslog',
-                    'priority' => 1,
                     'options' => [
-
                         'application' => 'pars-admin',
-                        'facility' => LOG_LOCAL0,
-                        'formatter' => [
-                            'name' => Laminas\Log\Formatter\Simple::class,
-                            'options' => [
-                                'format' => '%timestamp% %priorityName% (%priority%): %message% %extra%',
-                                'dateTimeFormat' => 'c',
-                            ],
-                        ],
-                        'filters' => [
-                            'priority' => [
-                                'name' => 'priority',
-                                'options' => [
-                                    'operator' => '<=',
-                                    'priority' => Laminas\Log\Logger::INFO,
-                                ],
-                            ],
-                        ],
                     ],
-                ],
-            ],
-            'processors' => [
-                'requestid' => [
-                    'name' => Laminas\Log\Processor\RequestId::class,
-                ],
-                'backtrace' => [
-                    'name' => Laminas\Log\Processor\Backtrace::class,
                 ],
             ],
         ],
