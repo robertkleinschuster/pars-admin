424b885a0fe6733ed9f17a032b5dc844ef39829e
robertkleinschuster <robert.kleinschuster@niceshops.com>
2021-05-18 19:03:18 +0200

    fix(Updates): Updates

--- test/ReachableTest.php
+++ test/ReachableTest.php
@@ -6,9 +6,30 @@ namespace ParsTest\Admin;
 
 use GuzzleHttp\Client;
 use Pars\Bean\PHPUnit\DefaultTestCase;
+use Pars\Core\Container\ParsContainer;
+use Pars\Core\Deployment\UpdaterInterface;
+use Psr\Container\ContainerInterface;
 
 class ReachableTest extends DefaultTestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $updater = $this->getContainer()->get(UpdaterInterface::class);
+        $updater->updateDB();
+    }
+
+    protected function getParsContainer(): ParsContainer
+    {
+        $container = $this->getContainer();
+        return $container->get(ParsContainer::class);
+    }
+
+    protected function getContainer(): ContainerInterface
+    {
+        return require 'config/container.php';
+    }
+
     /**
      * @group unit
      * @throws \GuzzleHttp\Exception\GuzzleException
@@ -18,5 +39,6 @@ class ReachableTest extends DefaultTestCase
         $client = new Client();
         $response = $client->get('localhost:9090');
         $this->assertEquals(200, $response->getStatusCode());
+        $this->assertContains('pars-admin-authentication-signinform', $response->getBody()->getContents());
     }
 }
