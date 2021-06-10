4d830ba679f50272164e950867e8f5ad6e3689d2
robertkleinschuster <robert.kleinschuster@niceshops.com>
2021-06-10 14:40:36 +0200

    feat(Admin): updater improvements

--- src/ApplicationContainer.php
+++ src/ApplicationContainer.php
@@ -5,7 +5,10 @@ namespace Pars\Admin;
 
 use Pars\Core\Application\AbstractApplicationContainer;
 
-
+/**
+ * Class ApplicationContainer
+ * @package Pars\Admin
+ */
 class ApplicationContainer extends AbstractApplicationContainer
 {
     public function getApplication()
